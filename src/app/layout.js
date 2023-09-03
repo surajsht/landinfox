90ce370406642a0568f8d595331eeb3dac21c6b9
suraj <surajsht2055@gmail.com>
2023-09-03 20:26:31 +0545

    navbar section done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["400", "500", "700", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "LandInfoX",
@@ -10,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
