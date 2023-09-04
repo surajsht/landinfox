76a473fa7909bbb858082a5626d90468d5db385b
suraj <surajsht2055@gmail.com>
2023-09-04 19:29:10 +0545

    made modification in file structure

---
+++ src/app/component/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
+const Navbar = () => {
+  let [toggle, setToggle] = useState(false);
+
+  const themeToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <nav className="container">
+      <div className="flex items-center justify-between py-4">
+        <h1 className="text-4xl font-black"> LandInfoX </h1>
+
+        <div
+          className="relative h-6 w-12 rounded-full bg-[#ccc] md:cursor-pointer"
+          onClick={() => themeToggle()}
+        >
+          <div
+            className={`absolute left-1 top-[50%] h-4 w-4 translate-y-[-50%] rounded-full bg-[#fff] transition duration-500 ${
+              toggle ? "translate-x-[23px]" : "translate-x-0"
+            }`}
+          ></div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
