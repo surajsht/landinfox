f6f5e47d41b577246be319e182f60fdb8b0291cf
suraj <surajsht2055@gmail.com>
2023-09-04 20:25:28 +0545

    select component basic design done

---
+++ src/app/component/switch/Switch.jsx
@@ -0,0 +1,16 @@
+const Switch = () => {
+  return (
+    <select className="cursor-pointer bg-[#1C49BB] p-3 pr-6 text-white">
+      <option value="all" selected>
+        Filter by Regin
+      </option>
+      <option value="africa"> Africa </option>
+      <option value="asia"> Asia </option>
+      <option value="europe"> Europe </option>
+      <option value="america"> America </option>
+      <option value="oceania"> Oceania </option>
+    </select>
+  );
+};
+
+export default Switch;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./component/navbar/Navbar";
 import Search from "./component/search/Search";
+import Switch from "./component/switch/Switch";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Search />
+      <Switch />
     </>
   );
 }
