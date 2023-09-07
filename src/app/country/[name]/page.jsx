6fe05c541ee8f3826a0becf90b33eac276154083
suraj <surajsht2055@gmail.com>
2023-09-07 21:10:40 +0545

    single page done

--- src/app/country/[name]/page.jsx
+++ src/app/country/[name]/page.jsx
@@ -1,9 +1,13 @@
 "use client";
 
+import Navbar from "@/app/component/navbar/Navbar";
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Single = ({ params }) => {
   let [countryData, setCountryData] = useState();
+  let [languages, setLanguages] = useState();
   let [loading, setLoading] = useState(true);
 
   let fetchSingleCountry = async () => {
@@ -24,17 +28,112 @@ const Single = ({ params }) => {
   }, []);
 
   return (
-    <div>
-      {loading ? (
-        <h2> Loading ... </h2>
-      ) : (
-        <div>
-          {countryData.map((data) => {
-            console.log(data);
-          })}
-        </div>
-      )}
-    </div>
+    <>
+      <Navbar />
+
+      <div>
+        {loading ? (
+          <h2> Loading ... </h2>
+        ) : (
+          <div className="container mb-4 mt-8 md:mt-24 lg:mt-36">
+            <button className="mb-10 bg-[#ccc] px-2 py-1">
+              <Link href="/" className="text-black">
+                Go back
+              </Link>
+            </button>
+
+            <div>
+              {countryData.map((data, dataIdx) => {
+                let {
+                  name,
+                  population,
+                  region,
+                  subregion,
+                  languages,
+                  landlocked,
+                  independent,
+                  flags,
+                  continents,
+                  capital,
+                  timezones,
+                } = data;
+
+                const languageValues = Object.values(languages);
+                const languageString = languageValues.join(", ");
+
+                return (
+                  <div key={dataIdx} className="md:flex md:gap-6 2xl:gap-10">
+                    <div className="relative mb-6 h-56 w-full sm:h-80 md:w-1/2 lg:w-[40%] 2xl:w-[30%]">
+                      <Image
+                        src={flags?.png}
+                        alt={name?.common}
+                        fill={true}
+                        priority={true}
+                        sizes="(max-width: 768px) 33vw, (max-width: 1200px) 20vw"
+                        className="object-cover sm:object-fill"
+                      />
+                    </div>
+
+                    <div className="md:w-1/2 lg:w-[60%] 2xl:w-[50%]">
+                      <h2 className="mb-6 text-2xl font-bold">
+                        {name?.common}
+                      </h2>
+
+                      <div className="space-y-2 md:flex md:flex-wrap md:items-center md:gap-4 md:space-y-0 lg:gap-x-0 lg:gap-y-4">
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Population : </span>
+                          {population}
+                        </div>
+
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Region : </span>
+                          {region}
+                        </div>
+
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Sub region : </span>
+                          {subregion}
+                        </div>
+
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Languages : </span>
+                          {languageString}
+                        </div>
+
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Landlocked : </span>
+                          {landlocked ? "true" : "false"}
+                        </div>
+
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Independent : </span>
+                          {independent ? "true" : "false"}
+                        </div>
+
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Continent : </span>
+                          {continents}
+                        </div>
+
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Capital : </span>
+                          {capital}
+                        </div>
+
+                        <div className="text-lg lg:w-[50%]">
+                          <span className="font-medium"> Timezone : </span>
+                          {timezones[0]}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
